2ea95c08f3d0fba3bf1244583a6ae7afd3b047fc
viveknapit <viveknapit12@gmail.com>
2023-08-22 23:18:02 +0530

    initial commit

---
+++ inputOutput/io.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int ammount1;
+    cin>>ammount1;
+    int ammount2;
+    cin>>ammount2;
+
+    int total = ammount1+ammount2;
+    cout<<total;
+    
+
+    return 0;
+}
+
